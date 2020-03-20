b76be0c43276daf826f15ced9f3923e327dfb585
Farukhzhon <fbarotov@sabanciuniv.edu>
2020-03-20 22:32:16 +0300

    minor modification

--- OnlineStore.Data/Models/Entities/Order.cs
+++ OnlineStore.Data/Models/Entities/Order.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace OnlineStore.Data.Models.Entities
 {
-    class Order
+    public class Order
     {
+        public int ID { get; set; }
+        public int CustomerId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public int TotalPrice { get; set; }
+        public string Address { get; set; }
+        public bool IsDelivered { get; set; }
     }
 }
