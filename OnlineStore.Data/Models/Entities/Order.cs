41f2dfe7cd48e357b70920da728faeb4606d7c0a
Farukhzhon <fbarotov@sabanciuniv.edu>
2020-03-20 22:17:11 +0300

    created data models and general template

---
+++ OnlineStore.Data/Models/Entities/Order.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineStore.Data.Models.Entities
+{
+    class Order
+    {
+    }
+}
---
+++ OnlineStore.Data/Models/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineStore.Data.Models.Entities
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int ModelNumber { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public string WarrantyStatus { get; set; }
+        public string DistributorInfo { get; set; }
+    }
+}
