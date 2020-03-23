e4f4b956a9126adee28869b4bab21eef3c9623d9
onatkaya <onatkaya@sabanciuniv.edu>
2020-03-23 22:32:24 +0300

    customer class

---
+++ OnlineStore.Data/Models/Entities/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineStore.Data.Models.Entities
+{
+    class Customer
+    {
+        public int customer_id { get; set; }
+        public string name { get; set; }
+        public int product_Id { get; set; }
+        public string email_address { get; set; }
+        public string home_address { get; set; }
+        public string password { get; set; }
+
+    }
+}
