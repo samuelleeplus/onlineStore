2636d7e9dd17addd99fa388c319630dfb13bf046
Farukhzhon <fbarotov@sabanciuniv.edu>
2020-03-24 20:13:50 +0300

    added base user

--- OnlineStore.Data/Models/Entities/Customer.cs
+++ OnlineStore.Data/Models/Entities/Customer.cs
@@ -4,14 +4,13 @@ using System.Text;
 
 namespace OnlineStore.Data.Models.Entities
 {
-    class Customer
+    class Customer : BaseUser
     {
         public int CustomerId { get; set; }
         public string Name { get; set; }
         public int ProductId { get; set; }
-        public string EmailAddress { get; set; }
         public string HomeAddress { get; set; }
         public string Password { get; set; }
-
+        public string Email { get; set; }
     }
 }
