c4d19d3cc9fa0cb9310da7e0558268323ec6f5ad
arautucu <arautucu@sabanciuniv.edu>
2020-03-30 19:40:28 +0300

    deliveryList class has been created

---
+++ OnlineStore.Data/Models/Entities/DeliveryList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/// <summary>
+/// Delivery list class provides delivery ID, customer ID, product UD, quantity, total price, delivery address, and is
+///delivered properties. These can be viewed by ProductManager.
+
+namespace OnlineStore.Data.Models.Entities
+{
+    public class deliveryList
+    {
+        public int deliveryId { get; set; }
+        //public int customerId { get; set; } already created in the Order class
+        public int productUd { get; set; }
+        public int quantity { get; set; }
+        public int totalPrice { get; set; }
+        public string deliveryAddress { get; set; }
+        public bool isDelivered { get; set; }
+    }
+}
