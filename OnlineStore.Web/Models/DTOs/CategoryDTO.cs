c689d1bf0f8279327207568219696453e82ee945
Farukhzhon <fbarotov@sabanciuniv.edu>
2020-04-06 18:30:54 +0300

    added ui and model for category

---
+++ OnlineStore.Web/Models/DTOs/CategoryDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Web.Models.DTOs
+{
+    public class CategoryDTO
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string BackgroundImageUrl { get; set; }
+        public IEnumerable<RelatedProduct> Products { get; set; }
+    }
+}
