373e50c48ff449c282b48bf96d491f6c4524dbd3
Samuel Lee <samuellee@sabanciuniv.edu>
2020-04-14 22:42:19 +0300

    minor

--- OnlineStore.Data/Models/Entities/Address.cs
+++ OnlineStore.Data/Models/Entities/Address.cs
@@ -7,6 +7,7 @@ namespace OnlineStore.Data.Models.Entities
     public class Address
     {
         public int Id { get; set; }
+        public int UserId { get; set; }
         public string StreetAddress { get; set; }
         public string City { get; set; }
         public string State { get; set; }
