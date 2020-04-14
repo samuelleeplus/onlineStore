b524e179ca183cafb53ace818d10d2fb5ac6d895
Samuel Lee <samuellee@sabanciuniv.edu>
2020-04-14 22:32:16 +0300

    latest

---
+++ OnlineStore.Data/Migrations/20200414165039_data.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineStore.Data.Migrations
+{
+    public partial class data : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ImageUris_Products_ProductId",
+                table: "ImageUris");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Distributors_DistributorId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_DistributorId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ImageUris_ProductId",
+                table: "ImageUris");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DistributorId",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "DistributorId",
+                table: "Products",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_DistributorId",
+                table: "Products",
+                column: "DistributorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ImageUris_ProductId",
+                table: "ImageUris",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ImageUris_Products_ProductId",
+                table: "ImageUris",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Distributors_DistributorId",
+                table: "Products",
+                column: "DistributorId",
+                principalTable: "Distributors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- OnlineStore.Data/Models/Entities/Order.cs
+++ OnlineStore.Data/Models/Entities/Order.cs
@@ -8,9 +8,8 @@ namespace OnlineStore.Data.Models.Entities
     public class Order
     {
         public int Id { get; set; }
-        public int CustomerId { get; set; }
-        public IEnumerable<OrderedProduct> OrderedProducts { get; set; }
-        public int TotalPrice { get; set; }
+        public int CustomerId { get; set; }      
+        public double TotalPrice { get; set; }
         public string Address { get; set; }
         public bool IsDelivered { get; set; }
     }
@@ -18,7 +17,11 @@ namespace OnlineStore.Data.Models.Entities
 
     public class OrderedProduct
     {
-        [Key]
+
+        public int Id { get; set; }
+       
+        public int OrderId { get; set; }
+        
         public int ProductId { get; set; }
         public int Quantity { get; set; }
     }
