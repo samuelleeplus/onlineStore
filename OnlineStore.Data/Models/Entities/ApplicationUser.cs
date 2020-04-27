c23cca187e560d6c03a6df2b4e71c5635a8e2f8d
Farukhzhon <fbarotov@sabanciuniv.edu>
2020-04-27 21:56:30 +0300

    with @onatKaya: added ui and model for checkout process

--- OnlineStore.Data/Models/Entities/ApplicationUser.cs
+++ OnlineStore.Data/Models/Entities/ApplicationUser.cs
@@ -8,5 +8,7 @@ namespace OnlineStore.Data.Models.Entities
     public class ApplicationUser : IdentityUser
     {
         public int TaxId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
---
+++ OnlineStore.Web/Models/DTOs/CheckoutDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Web.Models.DTOs
+{
+    public class CheckoutDto
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string AddressDetail { get; set; }
+        [Required]
+        public string ZipCode { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Province { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required] public bool AgreeTermsAndConditions { get; set; }
+
+    }
+}
